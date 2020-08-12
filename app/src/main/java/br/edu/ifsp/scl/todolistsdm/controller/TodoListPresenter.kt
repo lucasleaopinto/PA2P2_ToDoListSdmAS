842d97de0b9f151d098af09f5600d71c688932a8
Lucas Pinto <lucas.pinto@dxc.com>
2020-08-11 21:30:08 -0300

    TodoListSdm

---
+++ app/src/main/java/br/edu/ifsp/scl/todolistsdm/controller/TodoListPresenter.kt
@@ -0,0 +1,44 @@
+package br.edu.ifsp.scl.todolistsdm.controller
+
+import android.content.Context
+import androidx.room.Room
+import br.edu.ifsp.scl.todolistsdm.model.dao.TarefaDao
+import br.edu.ifsp.scl.todolistsdm.model.database.ToDoListDatabase
+import br.edu.ifsp.scl.todolistsdm.model.entity.Tarefa
+import br.edu.ifsp.scl.todolistsdm.view.TodoListViewInterface
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
+class TodoListPresenter(private val view: TodoListViewInterface) {
+
+    /* Também conhece o modelo, mas por injeção de dependência, isso não é um problema */
+    private val model: TarefaDao = Room.databaseBuilder(
+        (view as Context),
+        ToDoListDatabase::class.java,
+        ToDoListDatabase.Constantes.DB_NAME
+    ).build().getTarefaDao()
+
+    fun buscarTarefas() {
+        GlobalScope.launch {
+            val listaTarefas = model.recuperarTarefas().toMutableList()
+            view.setTarefas(listaTarefas)
+        }
+    }
+
+    fun apagarTarefa(tarefa: Tarefa) {
+        GlobalScope.launch {
+            model.removerTarefa(tarefa)
+        }
+    }
+
+    fun apagarTarefas(vararg tarefa: Tarefa) {
+        GlobalScope.launch {
+            model.removerTarefas(*tarefa)
+        }
+    }
+
+    fun alterarTarefa(tarefa: Tarefa) {
+        GlobalScope.launch { model.atualizarTarefa(tarefa) }
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/todolistsdm/view/TodoListViewInterface.kt
@@ -0,0 +1,10 @@
+package br.edu.ifsp.scl.todolistsdm.view
+
+import br.edu.ifsp.scl.todolistsdm.model.entity.Tarefa
+
+interface TodoListViewInterface {
+
+    fun setTarefas(listaTarefas: MutableList<Tarefa>)
+
+    fun setRetorno(tarefa: Tarefa)
+}
